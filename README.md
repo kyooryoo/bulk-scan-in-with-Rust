14a31ccfa8dc1130fc6bd3b79a05d240055239f9
Ling Jiang <kyooryoo@hotmail.com>
2022-01-01 14:45:26 +0800

    add flags

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # Bulk Scan In with Rust
 Implement Alma scan in API with Rust
 
-Test the program with `cargo run`:
-$ cargo run -- -k <YOURKEY> -f ./barcodes.txt
-Execute the program in your shell (Windows):
-$ scanin.exe -k <YOURKEY> -f ./barcodes.txt
+## Program flags
+- k --apikey default_value = "NOSUCHKEY"
+> You must provide APIKEY for the program to run
+- f --filepath default_value = "./barcodes.txt"
+> The text file holds barcodes to process
+- l --library default_value = "MAIN"
+> The library in which scan in is performed
+- c --circdesk default_value = "DEFAULT_CIRC_DESK"
+> The circulation desk where scan in happened
+- b --barcode default_value = "NOSUCHCODE"
+> Optional when you scan in only one item
+- i --inhouseuse default_value = "false")]
+> Optional if register scanned item for in house use
 
-Put *barcode.txt* aside *scanin.exe* in one folder.
-Put barcodes in *barcode.txt* one barcode per line.
+## Example of testing in dev mode:
+`$ cargo run -- -k YOURKEY -f ./barcodes.txt`
+## Example of executing the program in Windows:
+`$ scanin.exe -k YOURKEY -f ./barcodes.txt`
+
+## Notice
+* Put barcodes in *barcode.txt* one barcode per line.
+* Put *barcode.txt* with *scanin.exe* in the same folder.
+* You can `cargo build` executable for your own OS
+
+## Version
+* 0.1.0 first commit
+* 0.1.1 add flags
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[tokio::main]
- async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::from_args();
-
     let apikey = format!("apikey {}", args.apikey);
+    let barcode = args.barcode;
+    let param_op = String::from("op=SCAN");
+    let param_lib = format!("library={}", args.library);
+    let param_circ = format!("circ_desk={}", args.circdesk);
+    let param_house = format!("register_in_house_use={}", args.inhouseuse);
+
     let base_url = String::from("https://api-cn.hosted.exlibrisgroup.com.cn/almaws/v1");
     let barcode_url = String::from("/items?item_barcode=");
-    let client = reqwest::Client::builder().build()?;
 
-    let file = File::open(PathBuf::from(args.barcodes)).unwrap();
-    let reader = BufReader::new(file);
-    for (index, barcode) in reader.lines().enumerate() {
-        let barcode = String::from(barcode.unwrap());
+    if barcode != "NOSUCHCODE" {
+        let client = reqwest::Client::builder().build()?;
         let barcode_api = format!("{}{}{}", base_url, barcode_url, barcode);
         let res = client
             .get(barcode_api)
@@ -25,37 +29,79 @@ use structopt::StructOpt;
             .await?
             .text()
             .await?;
-    
         let js = json::parse(&res).unwrap();
         let mms_id = &js["bib_data"]["mms_id"];
         let holding_id = &js["holding_data"]["holding_id"];
         let item_id = &js["item_data"]["pid"];
-        let item_url = format!("{}/bibs/{}/holdings/{}/items/{}", base_url, mms_id, holding_id, item_id);
-    
-        let param_op = String::from("op=SCAN");
-        let param_lib = String::from("library=LYNN");
-        let param_circ = String::from("circ_desk=DEFAULT_CIRC_DESK");
-        let param_house = String::from("register_in_house_use=false");
-        let item_api = format!("{}?{}&{}&{}&{}", item_url, param_op, param_lib, param_circ, param_house);
-    
+        let item_url = format!(
+            "{}/bibs/{}/holdings/{}/items/{}",
+            base_url, mms_id, holding_id, item_id
+        );
+        let item_api = format!(
+            "{}?{}&{}&{}&{}",
+            item_url, param_op, param_lib, param_circ, param_house
+        );
         let res = client
-        .post(item_api)
-        .header("accept", "application/json")
-        .header("authorization", &apikey)
-        .send()
-        .await?;
+            .post(item_api)
+            .header("accept", "application/json")
+            .header("authorization", &apikey)
+            .send()
+            .await?;
         let post_status = res.status().is_success();
-    
-        let t = res
-        .text()
-        .await?;
+
+        let t = res.text().await?;
         let js = json::parse(&t).unwrap();
         let item_in_place = js["item_data"]["base_status"]["value"] == "1";
-        
-        if  post_status && item_in_place {
-            println!("No.{} processed: {}", index + 1, barcode);
+
+        if post_status && item_in_place {
+            println!("Item {} is processed!", barcode);
         } else {
             println!("Check the item {}!!!", barcode);
+        }
+    } else {
+        let file = File::open(PathBuf::from(args.barcodes)).unwrap();
+        let reader = BufReader::new(file);
+        for (index, barcode) in reader.lines().enumerate() {
+            let barcode = String::from(barcode.unwrap());
+            let client = reqwest::Client::builder().build()?;
+            let barcode_api = format!("{}{}{}", base_url, barcode_url, barcode);
+            let res = client
+                .get(barcode_api)
+                .header("accept", "application/json")
+                .header("authorization", &apikey)
+                .send()
+                .await?
+                .text()
+                .await?;
+            let js = json::parse(&res).unwrap();
+            let mms_id = &js["bib_data"]["mms_id"];
+            let holding_id = &js["holding_data"]["holding_id"];
+            let item_id = &js["item_data"]["pid"];
+            let item_url = format!(
+                "{}/bibs/{}/holdings/{}/items/{}",
+                base_url, mms_id, holding_id, item_id
+            );
+            let item_api = format!(
+                "{}?{}&{}&{}&{}",
+                item_url, param_op, param_lib, param_circ, param_house
+            );
+            let res = client
+                .post(item_api)
+                .header("accept", "application/json")
+                .header("authorization", &apikey)
+                .send()
+                .await?;
+            let post_status = res.status().is_success();
+
+            let t = res.text().await?;
+            let js = json::parse(&t).unwrap();
+            let item_in_place = js["item_data"]["base_status"]["value"] == "1";
+
+            if post_status && item_in_place {
+                println!("No.{} item processed: {}", index + 1, barcode);
+            } else {
+                println!("Check the item {}!!!", barcode);
+            }
         }
     }
 
@@ -64,9 +110,21 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Args {
-    #[structopt(short = "k", long = "apikey")]
+    #[structopt(short = "k", long = "apikey", default_value = "NOSUCHKEY")]
     apikey: String,
 
-    #[structopt(short = "f", long = "filepath")]
+    #[structopt(short = "f", long = "filepath", default_value = "./barcodes.txt")]
     barcodes: PathBuf,
+
+    #[structopt(short = "l", long = "library", default_value = "MAIN")]
+    library: String,
+
+    #[structopt(short = "c", long = "circdesk", default_value = "DEFAULT_CIRC_DESK")]
+    circdesk: String,
+
+    #[structopt(short = "b", long = "barcode", default_value = "NOSUCHCODE")]
+    barcode: String,
+
+    #[structopt(short = "i", long = "inhouseuse", default_value = "false")]
+    inhouseuse: String,
 }
